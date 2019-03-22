5901bf3fcdc22e4f50adcf86e4acf8a594d6c3a9
FireHunter3958 <47977209+FireHunter3958@users.noreply.github.com>
2019-03-22 12:12:28 +0000

    Add files via upload
    First Commit, all files.

---
+++ PriorityQueue/src/queuemanager/UnsortedLinkedPriorityQueue.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+/**
+ *
+ * @author 07014975
+ */
+public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+    
+    /**
+     * Where data is stored
+     */
+    private final Object[] storage;
+    
+    /**
+     * size of storage array
+     */
+    private final int capacity;
+    
+    /**
+     * The index of the last stored item
+     */
+    private int tailIndex;
+    
+    /**
+     * New empty queue of given size
+     * @param size
+     */
+    public UnsortedLinkedPriorityQueue(int size) {
+        storage = new Object[size];
+        capacity = size;
+        tailIndex = -1;
+    }
+    
+    @Override
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return ((PriorityItem<T>) storage[0]).getItem();
+        }
+        
+        /*
+        for (int i = 0; i < x; i++) {
+        
+        
+        */
+    }
+    
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        tailIndex = tailIndex + 1;
+        if (tailIndex >= capacity) {
+            tailIndex = tailIndex - 1;
+            throw new QueueOverflowException();
+        } else {
+            int i = tailIndex;
+            while (i > 0 && ((PriorityItem<T>) storage[i - 1]).getPriority() < priority) {
+                storage[i] = storage [i - 1];
+                i = i - 1;
+            }
+            storage[i] = new PriorityItem<>(item, priority);
+        }
+    }
+    
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            for (int i = 0; i < tailIndex; i++) {
+                storage[i] = storage[i + 1];
+            }
+            tailIndex = tailIndex - 1;
+        }
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return tailIndex < 0;
+    }
+    
+    @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i <= tailIndex; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + storage[i];
+        }
+        result = result + "]";
+        return result;
+    }
+}
